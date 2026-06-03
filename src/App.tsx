8c3f59071571a54e5a5a3155c9a0c05cefb859fa
Jcabot05 <stefano.cabot@uees.edu.ec>
2026-06-02 21:03:09 -0500

    Update dashboard layout and deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import { Grid } from '@mui/material';
-
+import Grid from '@mui/material/Unstable_Grid2'
 
 function App() {
   return (
-      <Grid container spacing={5} justifyContent="center" alignItems="center">
+    <Grid container spacing={5} sx={{ justifyContent: 'center', alignItems: 'center', padding: 2 }}>
 
-         {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      {/* Encabezado */}
+      <Grid xs={12} md={12}>Elemento: Encabezado</Grid>
 
-         {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+      {/* Alertas */}
+      <Grid xs={12} md={12}>Elemento: Alertas</Grid>
 
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+      {/* Selector */}
+      <Grid xs={12} md={3}>Elemento: Selector</Grid>
 
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+      {/* Indicadores */}
+      <Grid xs={12} md={9}>Elemento: Indicadores</Grid>
 
-         {/* Gráfico */}
-         <Grid 
-         size={{ xs: 12, md: 6 }}           
-          sx={{ display: { xs: "none", md: "block"} }} >
-          Elemento: Gráfico
-          </Grid>
+      {/* Gráfico */}
+      <Grid xs={12} md={6} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Gráfico</Grid>
 
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 6 }}
-          sx={{ display: { xs: "none", md: "block" } }}>
-          Elemento: Tabla
-          </Grid>
+      {/* Tabla */}
+      <Grid xs={12} md={6} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Tabla</Grid>
 
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+      {/* Información adicional */}
+      <Grid xs={12} md={12}>Elemento: Información adicional</Grid>
 
-      </Grid>
-    );
+    </Grid>
+  )
 }
 
 export default App
